9f370b841dc4cbdfb18765a0206be7886f8651e6
Jesse Schneider <jessewschneider96@gmail.com>
2020-05-10 21:21:21 +1000

    basic 2x2x2 nn mostly finished

--- neural-network.py
+++ neural-network.py
@@ -12,8 +12,8 @@ class NeuralNetwork():
         self.hidden_bias = [0.1, 0.1]
         self.output_bias = [0.1, 0.1]
         self.learning_rate = 3
-        self.n_epochs = 30
-        self.batch_size = 20
+        self.n_epochs = 1
+        self.batch_size = 2
         
 
     #sigmoid activation function
@@ -23,19 +23,6 @@ class NeuralNetwork():
     #sigmoid derivative function
     def sigmoid_derivative(self, x):
         return x * (1.0 - x)
-    
-    # #training function
-    # def train(self):
-    #     num_batches = round(self.input.shape[0]/ self.batch_size)
-
-    #     for i in range(self.epochs):
-    #         for j in range(self.batch_size):
-
-    #             batch = self.input[(j * self.batch_size):(j+1 * self.batch_size) - 1, ]
-    #             current_size = batch.shape[0]
-
-    #             sum_gradients = np.zeros((self.hi))
-
 
 
     def forward_pass(self, sample, target):
@@ -47,18 +34,18 @@ class NeuralNetwork():
                 hiddenL[i] = hiddenL[i] + (sample[j] * self.hiddenWeights[i][j])
         for i in range(len(hiddenL)):
             hiddenL[i] = self.sigmoid(hiddenL[i] + self.hidden_bias[i])
-        # print(hiddenL)
+        print("out_h: ", hiddenL)
 
         for i in range(len(outputL)):
             for j in range(len(sample)):
                 outputL[i] = outputL[i] + (hiddenL[j] * self.outputWeights[i][j])
         for i in range(len(outputL)):
             outputL[i] = self.sigmoid(outputL[i] + self.output_bias[i])
-        # print(outputL)
+        print("out_o: ", outputL)
 
         for i in range(len(error)):
             error[i] = (0.5 * ((target[i] - outputL[i])**2))
-        # print("error = ", error[0] + error[1])
+        print("error = ", error[0] + error[1])
         return hiddenL, outputL, error 
 
 
@@ -69,47 +56,31 @@ class NeuralNetwork():
         for i in range(hiddens):
             errorSum = 0
             for j in range(outputs):
-                error = -(target[j] - out_o[j]) * (out_o[j] * (1 - out_o[j])) * self.outputWeights[i][j]
+                error = -(target[j] - out_o[j]) * self.sigmoid_derivative(out_o[j]) * self.outputWeights[i][j]
                 errorSum += error
                 if j == outputs - 1:
                     error_outh.append(errorSum)
-        print(error_outh)
-
-        # e1_h1 = -(target[0] - out_o[0]) * (out_o[0] * (1 - out_o[0])) * self.outputWeights[0][0] #weight 5
-        # e2_h1 = -(target[1] - out_o[1]) * (out_o[1] * (1 - out_o[1])) * self.outputWeights[0][1] #weight 5
-
-        # e1_h2 = -(target[0] - out_o[0]) * (out_o[0] * (1 - out_o[0])) * self.outputWeights[1][0] #weight 7
-        # e2_h2 = -(target[1] - out_o[1]) * (out_o[1] * (1 - out_o[1])) * self.outputWeights[1][1] #weight 8
-        # e_outh1 = e1_h1 + e2_h1
-        # e_outh2 = e1_h2 + e2_h2
-        # error_outh = [e_outh1, e_outh2]
 
         for i in range(len(inputs)):
             for j in range(len(self.hiddenWeights[i])):
-                weight = error_outh[j] * (out_h[j] * (1 - out_h[j])) * inputs[i]
-                print("weights 1 - 4: ", format(weight, '.8f'))
+                weight = error_outh[j] * self.sigmoid_derivative(out_h[j]) * inputs[i]
                 weights.append(weight)
         
         for i in range(len(inputs)):
             for j in range(len(self.outputWeights[i])):
-                weight = -(target[j] - out_o[j]) * (out_o[j] * (1 - out_o[j])) * (out_h[i])
-                print("weights 5 - 8: ", format(weight, '.8f'))
+                weight = -(target[j] - out_o[j]) * self.sigmoid_derivative(out_o[j]) * (out_h[i])
                 weights.append(weight)
 
 
         for i in range(len(self.hidden_bias)):
-                weight = (out_h[i] * (1 - out_h[i])) * error_outh[i]
-                print("weights 9 - 10: ", format(weight, '.8f'))
+                weight = self.sigmoid_derivative(out_h[i]) * error_outh[i]
                 weights.append(weight)
 
         for i in range(len(self.output_bias)):
-                weight = (out_o[i] * (1 - out_o[i])) * error_outh[i]
-                print("weights 11 - 12: ", format(weight, '.8f'))
+                weight = self.sigmoid_derivative(out_o[i]) * error_outh[i]
                 weights.append(weight)
 
         return weights
-
-
 
 training_data = [[0.1, 0.1], [0.1, 0.2]]
 output_data = [[1, 0], [0, 1]]
@@ -125,9 +96,65 @@ outputs = 2
 # output_data = pickle.load(pickledata2)
 
 network = NeuralNetwork(inputs, hiddens, outputs)
-for i in range(inputs):
-    out_h, out_o, error = network.forward_pass(training_data[i], output_data[i])
-    gradients = network.backward_pass(out_h, out_o, output_data[i], training_data[i])
+num_batchs = len(training_data) // network.batch_size
+
+for epoch in range(network.n_epochs):
+    for batch in range(num_batchs):
+
+        current = training_data[(batch * network.batch_size): (batch + 1 * network.batch_size)]
+
+        current_size = len(current)
+
+        #initialise gradients sum and average arrays
+        gradients_sum = [0 for i in range((inputs * hiddens) + (hiddens * outputs) + hiddens + outputs)]
+        average_gradients = [0 for i in range((inputs * hiddens) + (hiddens * outputs) + hiddens + outputs)]
+
+        for i in range(len(current)):
+            out_h, out_o, error = network.forward_pass(training_data[i], output_data[i])
+            gradients = network.backward_pass(out_h, out_o, output_data[i], training_data[i])
+
+            #add gradients from back pass to sum total
+            for i in range(len(gradients)):
+                gradients_sum[i] += gradients[i]
+        
+        #get the average gradients from the batch
+        for i in range(len(average_gradients)):
+            average_gradients[i] = gradients_sum[i] / current_size
+
+        k = 0
+        for i in range(len(network.hiddenWeights)):
+            for j in range(len(network.hiddenWeights[i])):
+                if j % 2 == 1:
+                    k += 1
+                network.hiddenWeights[i][j] -= (network.learning_rate * average_gradients[k+j])
+                if j % 2 == 1:
+                    k -= 1
+            k += hiddens
+
+        for i in range(len(network.outputWeights)):
+            for j in range(len(network.outputWeights[i])):
+                if j % 2 == 1:
+                    k += 1
+                network.outputWeights[i][j] -= (network.learning_rate * average_gradients[k+j])
+                if j % 2 == 1:
+                    k -= 1
+            k += outputs
+
+        for i in range(len(network.hidden_bias)):
+            network.hidden_bias[i] -= (network.learning_rate * average_gradients[k+i])
+        k += hiddens
+
+        for i in range(len(network.output_bias)):
+            network.output_bias[i] -= (network.learning_rate * average_gradients[k+i])
+
+        print("hidden weights: ", network.hiddenWeights)
+        print("output weights: ", network.outputWeights)
+        print("hidden bias: ", network.hidden_bias)
+        print("output bias: ", network.output_bias)
+
+        
+        
+        
 
 
 
